17067b831b2f003f7dd2aa230f735d777cb8a468
Jack Ren <bjrjk@qq.com>
2017-01-28 13:45:29 +0800

    famous

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
 <?php
+$sql="SELECT * FROM `famous` ORDER by rand() limit 1";
+$result=mysql_query($sql,$conn);
+$row=mysql_fetch_array($result);
+$famous=$row['Content'];
+?>
+<br/><br/>
+<p><?php echo $famous; ?></p>
+<?php
 mysql_close();
 ?>
 </div>
